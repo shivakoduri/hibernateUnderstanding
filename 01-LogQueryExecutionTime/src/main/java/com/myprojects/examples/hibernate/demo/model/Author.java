f8f916dd63c83c300eca6fac98a2a320ba8d42b6
skoduri <shivaprasad.koduri@openet.com>
2018-04-19 10:33:39 +0100

    Adding entity

---
+++ 01-LogQueryExecutionTime/src/main/java/com/myprojects/examples/hibernate/demo/model/Author.java
@@ -0,0 +1,37 @@
+package com.myprojects.examples.hibernate.demo.model;
+
+public class Author {
+	
+	
+	private Long id;
+	private int version;
+	private String firstName;
+	private String lastName;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public int getVersion() {
+		return version;
+	}
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+
+}
