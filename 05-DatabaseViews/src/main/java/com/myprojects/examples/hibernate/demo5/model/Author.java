36ed83c5ca7a69453fe6c4aa38415dde73deb06c
shivakoduri <koduri5782>
2018-05-02 14:50:39 +0800

    Adding code database views

---
+++ 05-DatabaseViews/src/main/java/com/myprojects/examples/hibernate/demo5/model/Author.java
@@ -0,0 +1,98 @@
+package com.myprojects.examples.hibernate.demo5.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Version
+    private int version;
+
+    private String firstName;
+
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private List<Book> books = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Author)) {
+            return false;
+        }
+        Author other = (Author) obj;
+        if (id != null) {
+            if (!id.equals(other.id)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        String result = getClass().getSimpleName() + " ";
+        if (firstName != null && !firstName.trim().isEmpty())
+            result += "firstName: " + firstName;
+        if (lastName != null && !lastName.trim().isEmpty())
+            result += ", lastName: " + lastName;
+        return result;
+    }
+}
