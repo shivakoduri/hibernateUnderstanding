c6c915636d54fb66629c3c3e56389772539b3c90
shivakoduri <koduri5782>
2018-05-14 11:51:05 +0800

    Commit for MapOptionalAssociations

---
+++ 11-MapOptionalAssociations/src/main/java/com/myprojects/examples/hibernate/demo11/model/Book.java
@@ -0,0 +1,89 @@
+package com.myprojects.examples.hibernate.demo11.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Optional;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Version
+    private int version;
+
+    private String title;
+
+    @Temporal(TemporalType.DATE)
+    private Date publishingDate;
+
+    @ManyToOne
+    @JoinColumn(name = "publisherid")
+    private Publisher publisher;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public int getVersion() {
+        return this.version;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getPublishingDate() {
+        return publishingDate;
+    }
+
+    public void setPublishingDate(Date publishingDate) {
+        this.publishingDate = publishingDate;
+    }
+
+    public Optional<Publisher> getPublisher() {
+        return Optional.ofNullable(this.publisher);
+    }
+
+    public void setPublisher(final Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Book)) {
+            return false;
+        }
+        Book other = (Book) obj;
+        if (id != null) {
+            if (!id.equals(other.id)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        String result = getClass().getSimpleName() + " ";
+        if (title != null && !title.trim().isEmpty()) {
+            result += "title: " + title;
+        }
+        return result;
+    }
+}
